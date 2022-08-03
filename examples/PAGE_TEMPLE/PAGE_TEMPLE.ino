221fefda5e34b11726a05486785f82217d545cfe
Mengdu Li <mengdu.li@seeeed.cc>
2022-08-03 09:30:56 +0800

    Update:content

--- examples/PAGE_TEMPLE/PAGE_TEMPLE.ino
+++ examples/PAGE_TEMPLE/PAGE_TEMPLE.ino
@@ -24,20 +24,23 @@ void Sense_Display() // Sense interface display
     spr.setFreeFont(FSSB9);
     spr.setTextColor(TFT_BLACK, TFT_WHITE);
 
-    spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+    spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
     spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
     spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
-
-    spr.drawString("Sense", 32, 11, GFXFF);
+  
     spr.drawString("Process", 127, 11, GFXFF);
     spr.drawString("Network", 231, 11, GFXFF);
-
+  
+    spr.setTextColor(TFT_BLACK, tft.color565(135, 206, 235));
+    spr.drawString("Sense", 32, 11, GFXFF);
+  
+  
     spr.drawLine(0, 2 * FONT_ROW_HEIGHT, SCREEN_WIDTH, 2 * FONT_ROW_HEIGHT, TFT_WHITE);
-
+  
     spr.setFreeFont(FSS9);
     spr.setTextColor(TFT_BLACK, tft.color565(220, 220, 220));
-    spr.fillRect(127, 53.5, 16 * PIXEL, FONT_ROW_HEIGHT, tft.color565(220, 220, 220));
-    spr.drawString("Sensor", 130, 56, GFXFF);
+    spr.fillRect(128, 50, 16 * PIXEL, FONT_ROW_HEIGHT, tft.color565(220, 220, 220));
+    spr.drawString("Sensor", 132, 54, GFXFF);
 
     spr.pushSprite(0, 0);
     spr.deleteSprite();
