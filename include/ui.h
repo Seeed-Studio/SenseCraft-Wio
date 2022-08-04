d07b8d63b8331ef45653bceafc38860c6641f055
Baozhu Zuo <zuobaozhu@gmail.com>
2022-08-05 00:19:20 +0800

    ui: use thread to always reflash ui

--- include/ui.h
+++ include/ui.h
@@ -1,19 +1,34 @@
+#ifndef __UI_H__
+#define __UI_H__
+#include "Seeed_Arduino_ooFreeRTOS.h"
+#include <Arduino.h>
+#include "Free_Fonts.h"
 #include <stdint.h>
 #include <TFT_eSPI.h>
 
 
+using namespace cpp_freertos;
+
 #define SCREEN_WIDTH 320                        // Wio Terminal Maximum Width
 #define SCREEN_HIGH 240                         // Wio Terminal Maximum Height
 
-/*build a ui class with pages*/
-class UI
+
+class UI: public Thread
 {
 public:
-    UI();
-    ~UI();
+    UI(Message &m);
     void init();
+
+
+protected:
+    virtual void Run();
 
 private:
     TFT_eSPI tft;
     TFT_eSprite spr = TFT_eSprite(&tft);
+
+    Message &Mail;
+    uint8_t buff[256];
 };
+
+#endif // __UI_H__
--- src/ui.cpp
+++ src/ui.cpp
@@ -1,18 +1,32 @@
 #include "ui.h"
-#include <Arduino.h>
-#include "Free_Fonts.h"
-UI::UI()
-{
-}
 
-UI::~UI()
+UI::UI(Message &m)
+    : Thread("UIThread", 256, 1),
+      Mail(m)
 {
-}
+    Start();
+};
+
 void UI::init()
 {
     tft.begin();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
-    
+
     spr.createSprite(SCREEN_WIDTH, SCREEN_HIGH);
+}
+
+void UI::Run(){
+    uint8_t nums;
+
+    while (true) {
+      nums = Mail.Receive(&buff, 256, 0);
+      if (nums > 0){
+        Serial.printf("Receive: %d ", nums);
+        Serial.println(buff[0]);
+      }
+      else
+        Serial.println("Receive: 0");
+        Delay(Ticks::MsToTicks(100));
+    }
 }
