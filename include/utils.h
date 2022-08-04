cc363d5c6e662d162ff9f8c9f1fc723de3ee4157
Baozhu Zuo <zuobaozhu@gmail.com>
2022-08-05 00:18:24 +0800

    utils: collect all usefull info

---
+++ include/utils.h
@@ -0,0 +1,15 @@
+#ifndef __UTILS_H__
+#define __UTILS_H__
+#include <stdint.h>
+enum button_state {
+    SENSE_PRESSED = 1,
+    PROCESS_PRESSED,
+    NETWORK_PRESSED,
+    UP_PRESSED,
+    DOWN_PRESSED,
+    LEFT_PRESSED,
+    RIGHT_PRESSED,
+    SELECT_PRESSED,
+};
+
+#endif // __UTILS_H__
