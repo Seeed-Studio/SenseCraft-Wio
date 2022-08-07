e01060de6fa5ca5107dd0e7ecc6c7b9f5d08d4ce
Baozhu Zuo <zuobaozhu@gmail.com>
2022-08-07 14:26:44 +0800

    sensor:add build LIS3DHTR sensor

---
+++ src/sensors/LIS3DHTRSensor.cpp
@@ -0,0 +1,48 @@
+#include "LIS3DHTRSensor.h"
+#include "SensorsUtils.h"
+
+LIS3DHTRSensor::LIS3DHTRSensor() {
+}
+
+void LIS3DHTRSensor::init() {
+    if (I2CScanner(LIS3DHTR_DEFAULT_ADDRESS, Wire1)) {
+        lis.begin(Wire1);
+        lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ); // Data output rate
+        lis.setFullScaleRange(LIS3DHTR_RANGE_2G);
+        status = true;
+    } else {
+        status = false;
+    }
+}
+
+bool LIS3DHTRSensor::read(struct sensor_data *sdata) {
+    float x_values, y_values, z_values;
+
+    sdata->id   = LIS3DHTRSENSOR;
+    sdata->name = name;
+
+    if (status) {
+        if (!I2CScanner(LIS3DHTR_DEFAULT_ADDRESS, Wire1)) {
+            status = false;
+            return false;
+        }
+
+        lis.getAcceleration(&x_values, &y_values, &z_values);
+        data[0]       = (int)(x_values * 100);
+        data[1]       = (int)(y_values * 100);
+        data[2]       = (int)(z_values * 100);
+        sdata->data   = data;
+        sdata->size   = sizeof(data);
+        sdata->status = true;
+        return true;
+    } else {
+        sdata->data   = NULL;
+        sdata->status = false;
+        init(); // init again, try to find the sensor
+        return false;
+    }
+}
+
+const char *LIS3DHTRSensor::get_name() {
+    return this->name;
+}
---
+++ src/sensors/LIS3DHTRSensor.h
@@ -0,0 +1,26 @@
+#ifndef __LIS3DHTRSENSOR_H__
+#define __LIS3DHTRSENSOR_H__
+#include "sensor.h"
+#include "utils.h"
+#include "Wire.h"
+#include "LIS3DHTR.h"
+
+
+
+class LIS3DHTRSensor : public sensor_base
+{
+public:
+    LIS3DHTRSensor();
+    void init();
+    bool read(struct sensor_data *data);
+    const char *get_name();
+private:
+    const char *name = "LIS3DHTR";
+    int data[3];
+
+    bool status;
+
+    LIS3DHTR<TwoWire> lis;
+};
+
+#endif // __LIS3DHTRSENSOR_H__
