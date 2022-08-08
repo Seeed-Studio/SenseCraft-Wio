c8166245b5751994269f1c8d327dce13087fae45
Mengdu Li <mengdu.li@seeeed.cc>
2022-08-08 14:45:44 +0800

    Add: pages

---
+++ test_display.ino
@@ -0,0 +1,226 @@
+#include <TFT_eSPI.h> 
+#include "Free_Fonts.h"
+#include"LIS3DHTR.h"
+
+LIS3DHTR<TwoWire> lis;
+
+TFT_eSPI tft;
+TFT_eSprite spr = TFT_eSprite(&tft); 
+
+#define SCREEN_WIDTH 320                        // Wio Terminal Maximum Width
+#define SCREEN_HIGH 240                         // Wio Terminal Maximum Height
+#define PIXEL 4                                 // Width of one letter
+#define LEFT_SIDE 70
+#define HIGHT_SIDE 47
+const static unsigned int FONT_ROW_HEIGHT = 22; // The height of a letter
+
+
+void DISPLAY_INIT()            // Display initialization, black background rotation
+{
+    tft.begin();
+    tft.setRotation(3);
+    tft.fillScreen(TFT_BLACK);
+}
+
+void top(int _CHOOSE)
+{
+  spr.createSprite(SCREEN_WIDTH, 2 * FONT_ROW_HEIGHT + 1);
+
+  if(_CHOOSE == 1)spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+  else spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+  if(_CHOOSE == 2)spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+  else spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+  if(_CHOOSE == 3)spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+  else spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+
+  spr.setFreeFont(FSSB9);
+  spr.setTextColor(TFT_BLACK);
+  spr.drawString("Sense", 32, 11, GFXFF);
+  spr.drawString("Process", 127, 11, GFXFF);
+  spr.drawString("Network", 231, 11, GFXFF);
+
+  spr.drawLine(0, 2 * FONT_ROW_HEIGHT, SCREEN_WIDTH, 2 * FONT_ROW_HEIGHT, TFT_WHITE);
+
+  spr.pushSprite(0, 0);
+  spr.deleteSprite();
+}
+
+void Sound_Display(int sound)
+{
+  spr.createSprite(24 * PIXEL, 3.7 * FONT_ROW_HEIGHT);
+
+  spr.setFreeFont(FSS9);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawString("Sound", 20, 0, GFXFF);
+
+  spr.setFreeFont(FSS24);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawNumber(sound, 0, 2 * FONT_ROW_HEIGHT, GFXFF);
+  spr.pushSprite(10, 3.8 * FONT_ROW_HEIGHT);
+  spr.deleteSprite();
+}
+
+void IMU_Display(float x, float y, float z)
+{
+  spr.createSprite(27 * PIXEL, 7 * FONT_ROW_HEIGHT);
+
+  spr.setFreeFont(FSS9);
+  spr.setTextColor(TFT_BLACK, tft.color565(220, 220, 220));
+  spr.fillRect(0, 0, 16 * PIXEL, FONT_ROW_HEIGHT, tft.color565(220, 220, 220));
+  spr.drawString("Sensor", 2, 2, GFXFF);
+
+  spr.setFreeFont(FSS9);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawString("IMU", 15, 12 + FONT_ROW_HEIGHT, GFXFF);
+
+  spr.setFreeFont(FSS12);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawFloat(x, 2, 10, 2.5 * FONT_ROW_HEIGHT, GFXFF);       //Display the value of IMU X-axis
+  spr.drawFloat(y, 2, 10, 3.5 * FONT_ROW_HEIGHT, GFXFF);       //Display the value of IMU Y-axis
+  spr.drawFloat(z, 2, 10, 4.5 * FONT_ROW_HEIGHT, GFXFF);       //Display the value of IMU Z-axis
+  spr.setFreeFont(FSS9);
+  spr.drawString("X,Y,Z", 10, 5.5 * FONT_ROW_HEIGHT, GFXFF);
+
+  spr.fillCircle(17, 6.8 * FONT_ROW_HEIGHT, 3, tft.color565(0, 193, 255));
+  spr.fillCircle(34, 6.8 * FONT_ROW_HEIGHT, 3, tft.color565(220, 220, 220));
+  
+  spr.pushSprite(10 + 30 * PIXEL, 2.2 * FONT_ROW_HEIGHT);
+  spr.deleteSprite();
+}
+
+void Add_Display()
+{
+  spr.createSprite(27 * PIXEL, 3.7 * FONT_ROW_HEIGHT);
+
+  spr.setFreeFont(FSS9);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawString("Add", 2, 0, GFXFF);
+  
+  spr.fillRect(0, 1.5 * FONT_ROW_HEIGHT, 40, 40, TFT_WHITE);
+  spr.fillRect(19, 1.5 * FONT_ROW_HEIGHT + 6, 3, 26, TFT_BLACK);
+  spr.fillRect(7, 1.5 * FONT_ROW_HEIGHT + 18, 26, 3, TFT_BLACK);
+
+  spr.pushSprite(10 + 59 * PIXEL, 3.8 * FONT_ROW_HEIGHT);
+  spr.deleteSprite();
+}
+
+void Sense_AutoDetecte_Display(int _CHOOSE)                      //Display screen for accessing sensors
+{
+  spr.createSprite(SCREEN_WIDTH, SCREEN_HIGH);
+
+  // put your main code here
+  spr.setFreeFont(FSSB9);
+  spr.setTextColor(TFT_BLACK, TFT_WHITE);
+
+  spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+  spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+  spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+
+  spr.drawString("Process", 127, 11, GFXFF);
+  spr.drawString("Network", 231, 11, GFXFF);
+
+  spr.setTextColor(TFT_BLACK, tft.color565(135, 206, 235));
+  spr.drawString("Sense", 32, 11, GFXFF);
+
+  spr.drawLine(0, 2 * FONT_ROW_HEIGHT, SCREEN_WIDTH, 2 * FONT_ROW_HEIGHT, TFT_WHITE);
+
+  spr.setFreeFont(FSS9);
+  spr.setTextColor(TFT_BLACK, tft.color565(220, 220, 220));
+  spr.fillRect(128, 50, 16 * PIXEL, FONT_ROW_HEIGHT, tft.color565(220, 220, 220));
+  spr.drawString("Sensor", 132, 54, GFXFF);
+  
+//_CHOOSE = 2;
+   switch(_CHOOSE){
+    case 1: 
+      spr.fillRect(125, 80, 65, 110, TFT_GREEN);/// GREEN BLOCK 1st
+      break;
+    case 2:
+      spr.fillRect(240, 80, 48, 80, TFT_GREEN);
+      break;
+    default:
+      break;
+  }
+  spr.setTextColor(TFT_WHITE);
+  spr.drawString("Sound", 100 - LEFT_SIDE, 80, GFXFF);
+  spr.drawString("IMU", 209 - LEFT_SIDE, 80, GFXFF);
+  
+//  spr.fillRect(310 - LEFT_SIDE, 80, 12 * PIXEL, 80, TFT_GREEN);
+  spr.drawString("ADD", 316 - LEFT_SIDE, 80, GFXFF);
+
+  
+  spr.setFreeFont(FSS24);
+  spr.setTextColor(TFT_WHITE);
+  spr.drawString("356", 20, 115 , GFXFF);                      // Display the value of loudness
+  spr.setFreeFont(FSS12);
+  
+  spr.drawString("0.10", 205 - LEFT_SIDE, 102 , GFXFF);        //Display the value of IMU X-axis
+  spr.drawString("-0.23", 205 - LEFT_SIDE, 126 , GFXFF);       //Display the value of IMU Y-axis
+  spr.drawString("-2.33", 205 - LEFT_SIDE, 150 , GFXFF);       //Display the value of IMU Z-axis
+  
+  spr.setFreeFont(FSS9);
+  spr.drawString("X,Y,Z", 205 - LEFT_SIDE, 174 , GFXFF);
+
+  spr.fillCircle(163, 200, 3, tft.color565(0, 193, 255));
+  spr.fillCircle(146, 200, 3, tft.color565(220, 220, 220));spr.fillCircle(163, 200, 3, tft.color565(0, 193, 255));
+  spr.fillCircle(146, 200, 3, tft.color565(220, 220, 220));
+
+  spr.fillRect(244, 117, 40, 40, TFT_WHITE);
+  spr.fillRect(263, 123, 3, 26, TFT_BLACK);
+  spr.fillRect(251, 135, 26, 3, TFT_BLACK);
+  
+  spr.drawString("Network :", 5, 218 , GFXFF);
+
+//  spr.setTextColor(TFT_RED, TFT_BLACK);                      //Networking status indication：OFF
+//  spr.drawString("OFF", 82, 218 , GFXFF);
+
+  spr.setTextColor(TFT_GREEN, TFT_BLACK);             //Networking status indication：ON
+  spr.drawString("LoRa(SenseCAP)", 82, 218 , GFXFF);  //Show the network you are in
+
+  spr.pushSprite(0, 0);
+  spr.deleteSprite();
+}
+
+void Network_state(int s_key)
+{
+    spr.createSprite(320, 25);
+    spr.setFreeFont(FSSB9);
+    spr.fillSprite(TFT_BLACK);
+
+    switch (s_key)
+    {
+    case 0:
+        spr.setTextColor(TFT_RED);
+        spr.drawString("OFF", 90, 0, GFXFF);
+        break;
+    case 1:
+        spr.setTextColor(TFT_GREEN, TFT_BLACK);           // Networking status indication：ON
+        spr.drawString("LoRa(SenseCAP)", 90, 0, GFXFF); // Show the network you are in
+        break;
+    case 2:
+        spr.setTextColor(TFT_GREEN, TFT_BLACK);          // Networking status indication：ON
+        spr.drawString("WIFI(Ubidots)", 90, 0, GFXFF); // Show the network you are in
+        break;
+    default:;
+    }
+    spr.setTextColor(TFT_WHITE);
+    spr.drawString("Network :", 5, 0, GFXFF);
+    spr.pushSprite(0, 215);
+    spr.deleteSprite();
+}
+
+void setup() {
+  DISPLAY_INIT();
+  pinMode(WIO_MIC, INPUT);
+  lis.begin(Wire1);
+  lis.setOutputDataRate(LIS3DHTR_DATARATE_25HZ);
+  lis.setFullScaleRange(LIS3DHTR_RANGE_2G);
+}
+
+void loop() {
+  Network_state(1);
+  top(1);
+  Sound_Display(analogRead(WIO_MIC));
+  IMU_Display(lis.getAccelerationX(), lis.getAccelerationY(), lis.getAccelerationZ());
+  Add_Display();
+  delay(500);
+}
