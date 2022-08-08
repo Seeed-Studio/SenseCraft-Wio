d641edaa9683ae82fb3671faf39fe45de6aa403b
w0x7ce <tianrui.wang@seeed.cc>
2022-08-08 14:33:38 +0800

    fix: exmaple

--- examples/PAGE_TEMPLE/PAGE_TEMPLE.ino
+++ examples/PAGE_TEMPLE/PAGE_TEMPLE.ino
@@ -11,62 +11,80 @@ const static unsigned int FONT_ROW_HEIGHT = 22; // The height of a letter
 
 void DISPLAY_INIT() // Display initialization, black background rotation
 {
+    pinMode(A0, INPUT);
+    pinMode(WIO_LIGHT, INPUT);
+    pinMode(WIO_KEY_A, INPUT_PULLUP);
+    pinMode(WIO_KEY_B, INPUT_PULLUP);
+    pinMode(WIO_KEY_C, INPUT_PULLUP);
     tft.begin();
     tft.setRotation(3);
     tft.fillScreen(TFT_BLACK);
 }
 
+// 320 * 25 = 8000
 void Network_state(int s_key)
 {
+    spr.createSprite(320, 25);
+    spr.setFreeFont(FSSB9);
+    spr.fillSprite(TFT_BLACK);
+
     switch (s_key)
     {
     case 0:
         spr.setTextColor(TFT_RED);
-        spr.drawString("OFF", 82, 218, GFXFF);
+        spr.drawString("OFF", 90, 0, GFXFF);
         break;
     case 1:
         spr.setTextColor(TFT_GREEN, TFT_BLACK);           // Networking status indication：ON
-        spr.drawString("LoRa(SenseCAP)", 82, 218, GFXFF); // Show the network you are in
+        spr.drawString("LoRa(SenseCAP)", 90, 0, GFXFF); // Show the network you are in
         break;
     case 2:
         spr.setTextColor(TFT_GREEN, TFT_BLACK);          // Networking status indication：ON
-        spr.drawString("WIFI(Ubidots)", 82, 218, GFXFF); // Show the network you are in
+        spr.drawString("WIFI(Ubidots)", 90, 0, GFXFF); // Show the network you are in
         break;
     default:;
     }
     spr.setTextColor(TFT_WHITE);
-    spr.drawString("Network :", 5, 218, GFXFF);
+    spr.drawString("Network :", 5, 0, GFXFF);
+    spr.pushSprite(0, 215);
+    spr.deleteSprite();
 }
 
 int GG;
-void Sense_Display() // Sense interface display
+
+//320*70 = 22400
+void Sense_Display(int CHOOSE_PAGE) // Sense interface display
 {
-    spr.createSprite(SCREEN_WIDTH, SCREEN_HIGH);
-
-    // put your main code here
+    spr.createSprite(320, 70);
     spr.setFreeFont(FSSB9);
-    spr.setTextColor(TFT_BLACK, TFT_WHITE);
+    switch (CHOOSE_PAGE)
+    {
+    case 0:
+        spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+        spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        break;
+    case 1:
+        spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+        spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        break;
+    case 2:
+        spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
+        spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
+        break;
+    default:
+        break;
+    }
 
-    spr.fillRect(4 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, tft.color565(135, 206, 235));
-    spr.fillRect(30 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
-    spr.fillRect(56 * PIXEL, 0, 21 * PIXEL, FONT_ROW_HEIGHT + 15, TFT_WHITE);
-
+    spr.setFreeFont(FSSB9);
+    spr.setTextColor(TFT_BLACK);
+    spr.drawString("Sense", 32, 11, GFXFF);
     spr.drawString("Process", 127, 11, GFXFF);
     spr.drawString("Network", 231, 11, GFXFF);
-
-    spr.setTextColor(TFT_BLACK, tft.color565(135, 206, 235));
-    spr.drawString("Sense", 32, 11, GFXFF);
-
     spr.drawLine(0, 2 * FONT_ROW_HEIGHT, SCREEN_WIDTH, 2 * FONT_ROW_HEIGHT, TFT_WHITE);
 
-    spr.setFreeFont(FSS9);
-    spr.setTextColor(TFT_BLACK, tft.color565(220, 220, 220));
-    spr.fillRect(128, 50, 16 * PIXEL, FONT_ROW_HEIGHT, tft.color565(220, 220, 220));
-    spr.drawString("Sensor", 132, 54, GFXFF);
-
-    Network_state(GG % 3);
-    GG++;
-    delay(1000);
     spr.pushSprite(0, 0);
     spr.deleteSprite();
 }
@@ -76,7 +94,23 @@ void setup()
     DISPLAY_INIT();
 }
 
+int test_flag = 0;
+int test_flag_1 = 0;
+
 void loop()
 {
-    Sense_Display();
+    if (digitalRead(WIO_KEY_A) == LOW){
+       test_flag_1++;
+       delay(300);
+    }
+
+    Sense_Display(test_flag_1%3);
+
+    if (digitalRead(WIO_KEY_B) == LOW){
+       test_flag++;
+       delay(300);
+    }
+
+    Network_state(test_flag%3);
+    delay(20);
 }
