52b4b5b20a2e1928db197c21cb14d20d589c999d
IsQianGe <2716275834@qq.com>
2022-09-06 14:13:33 +0800

    The unit of the sound sensor display is dB

--- src/sensors/buildin_mic.cpp
+++ src/sensors/buildin_mic.cpp
@@ -8,6 +8,10 @@ void buildin_mic::init() {
 }
 bool buildin_mic::read(struct sensor_data *sdata) {
     mic_value        = analogRead(WIO_MIC);
+    if(mic_value < 250)
+        mic_value = 0;
+    else
+        mic_value = (uint32_t)((mic_value - 250) / 3.19);
     sdata->data_type = SENSOR_DATA_TYPE_INT32;
     sdata->data      = &mic_value;
     sdata->size      = sizeof(mic_value);
